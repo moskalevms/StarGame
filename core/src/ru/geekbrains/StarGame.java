56aa27de5220382846d40b642535c9d2560f0f39
moskalevms <45715336+moskalevms@users.noreply.github.com>
2019-04-07 12:15:18 +0300

    Update StarGame.java

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -6,6 +6,7 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
+//чтобы сделать PR
 public class StarGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
