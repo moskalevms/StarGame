13dcf285ea40da1fe850ebe381e741d8395733ba
moskalevms <mis-van@yandex.ru>
2019-04-10 20:52:00 +0300

    Выполнил ДЗ по второму уроку

--- core/src/ru/geekbrains/StarGame.java
+++ core/src/ru/geekbrains/StarGame.java
@@ -1,34 +1,19 @@
 package ru.geekbrains;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("space.png");
-	}
+import ru.geekbrains.screen.MenuScreen;
+
+public class StarGame extends Game {
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 1, 1, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img,0,0);
-		batch.setColor(0.177787f, 0.15678f, 0.15653645f,0.5656541f);
-		batch.end();
+	public void create () {
+		setScreen(new MenuScreen());
 	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+
 }
