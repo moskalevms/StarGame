c7c20aa5574331ddf008be2bf97df3e117b0315b
moskalevms <mis-van@yandex.ru>
2019-04-14 15:10:54 +0300

    Доработка программы после 3-го урока

---
+++ core/src/ru/geekbrains/math/MatrixUtils.java
@@ -0,0 +1,38 @@
+package ru.geekbrains.math;
+
+import com.badlogic.gdx.math.Matrix3;
+import com.badlogic.gdx.math.Matrix4;
+
+/**
+ * Утилита для работы с матрицами
+ */
+public class MatrixUtils {
+
+    private MatrixUtils() {
+    }
+
+    /**
+     * Расчёт матрицы перехода 4x4
+     * @param mat итоговая матрица преобразований
+     * @param src исходный квадрат
+     * @param dst итоговый квадрат
+     */
+    public static void calcTransitionMatrix(Matrix4 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth(); //как относится щирина исходного прямоугольника к требуемому прямоугольнику
+        float scaleY = dst.getHeight() / src.getHeight(); //как относится высота исходного прямоугольника к требуемому прямоугольнику
+        //делаем матрицу единичной -> translate: смещаем исходную матрицу в начало координат -> скалируем -> перемещаем
+        mat.idt().translate(dst.pos.x, dst.pos.y, 0f).scale(scaleX, scaleY, 1f).translate(-src.pos.x, -src.pos.y, 0f);
+    }
+
+    /**
+     * Расчёт матрицы перехода 3x3
+     * @param mat итоговая матрица преобразований
+     * @param src исходный квадрат
+     * @param dst итоговый квадрат
+     */
+    public static void calcTransitionMatrix(Matrix3 mat, Rect src, Rect dst) {
+        float scaleX = dst.getWidth() / src.getWidth();
+        float scaleY = dst.getHeight() / src.getHeight();
+        mat.idt().translate(dst.pos.x, dst.pos.y).scale(scaleX, scaleY).translate(-src.pos.x, -src.pos.y);
+    }
+}
---
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -0,0 +1,48 @@
+package ru.geekbrains.screen;
+
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
+
+public class MenuScreen extends BaseScreen {
+
+    private Texture bg;
+    private Background background;
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("space.png");
+        background = new Background(new TextureRegion(bg));
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        batch.begin();
+        background.draw(batch);
+        batch.end();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer) {
+        return false;
+    }
+}
